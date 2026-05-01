02b9787518bcbd9eabd2aee3a7072d26c55d0949
Manal Thakur <thakurmanal398@gmail.com>
2026-05-01 17:01:24 +0530

    changes

--- testing.py
+++ testing.py
@@ -1,1 +1,6 @@
-print ("hello updated world")
+import os
+
+os.system("git add .")
+os.system('git commit -m "update files"')
+os.system("git pull origin master --rebase")
+os.system("git push origin master")
